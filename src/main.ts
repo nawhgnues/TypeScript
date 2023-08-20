ba7647a78a4ef2b7deb0f1037db72e90428a38d1
nawhgnues <seungdang123jp.gmail.com>
2023-08-20 23:03:00 +0900

    Nested API 테스트

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import axios, { AxiosResponse } from "axios";
 
 //interface
+
+interface Geo {
+  lat: String;
+  lng: String;
+}
+
+interface Address {
+  city: String;
+  geo: Geo;
+  street: String;
+  suite: String;
+  zipcode: String;
+}
+
+interface Company {
+  bs: String;
+  catchPhrase: String;
+  name: String;
+}
+
 interface User {
+  address: Address;
+  company: Company;
   email: String;
   id: Number;
   name: String;
@@ -18,8 +40,23 @@ const res: AxiosResponse = await axios
 
 res.data.map((data: User) =>
   users.push({
+    address: {
+      city: `${data.address.city}🏠`,
+      geo: {
+        lat: data.address.geo.lat,
+        lng: data.address.geo.lng,
+      },
+      street: data.address.street,
+      suite: data.address.suite,
+      zipcode: data.address.zipcode,
+    },
+    company: {
+      bs: data.company.bs,
+      catchPhrase: data.company.catchPhrase,
+      name: data.company.name,
+    },
     id: data.id,
-    name: `${data.name}!`,
+    name: `${data.name}🤍`,
     username: data.username,
     email: data.email,
     phone: data.phone,
